1339e96f791250219ae6d1bce57436084bee2fe9
Robert Skalko <treborx555@gmail.com>
2024-08-10 14:29:01 +0200

    more log tweaks

--- src/main/java/com/robertx22/library_of_exile/main/ExileLog.java
+++ src/main/java/com/robertx22/library_of_exile/main/ExileLog.java
@@ -2,6 +2,7 @@ package com.robertx22.library_of_exile.main;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.util.StackLocatorUtil;
 
 // always use this to log stuff
 public class ExileLog {
@@ -9,20 +10,14 @@ public class ExileLog {
     private final Logger LOGGER;
 
     public ExileLog() {
-        this.LOGGER = LogManager.getLogger();
+        var name = LogManager.getLogger(StackLocatorUtil.getCallerClass(2));
+        this.LOGGER = LogManager.getLogger("Exile Log: " + name.getName());
     }
 
-    public ExileLog(String ref) {
-        this.LOGGER = LogManager.getLogger(ref);
-    }
-
-    public static ExileLog getWithRef(String ref){
-        return new ExileLog(ref);
-    }
-
-    public void warn(String str, Object... obj){
+    public void warn(String str, Object... obj) {
         LOGGER.warn(str, obj);
     }
+
     public void log(String str, Object... obj) {
         LOGGER.info(str, obj);
     }
--- src/main/java/com/robertx22/library_of_exile/registry/JsonExileRegistry.java
+++ src/main/java/com/robertx22/library_of_exile/registry/JsonExileRegistry.java
@@ -41,7 +41,7 @@ public interface JsonExileRegistry<T> extends ExileRegistry<T> {
         NOT_LOADED_JSONS_MAP.get(type).add(id);
     }
 
-    ExileLog LOGGER = ExileLog.getWithRef("Mine and Slash");
+    ExileLog LOGGER = new ExileLog();
 
     @Override
     default void compareLoadedJsonAndFinalClass(JsonObject json, Boolean editmode) {
